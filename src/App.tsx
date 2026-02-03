47a3e1b84057d680e88c2599b40602ba28893338
Yurii <lobacovurij@gmail.com>
2026-02-03 20:28:10 +0100

    added first base layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import PartlyCloudy from "./assets/cloudy.png";
+import Sunny from "./assets/sun.png";
+import Rain from "./assets/heavy-rain.png";
+import Snow from "./assets/snow.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="main-card">
+        <h3>Chemnnitz</h3>
+        <h4>Montag</h4>
+        <img
+          src={PartlyCloudy}
+          alt="weather image"
+          width="128px"
+          height="128px"
+        />
+        <p>Today is {} C</p>
+        <p></p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="card-group">
+        <div className="card">
+          <h4>Montag</h4>
+          <img
+            src={PartlyCloudy}
+            alt="weather image"
+            width="64px"
+            height="64px"
+          />
+          <p>Today is {} C</p>
+          <p></p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
